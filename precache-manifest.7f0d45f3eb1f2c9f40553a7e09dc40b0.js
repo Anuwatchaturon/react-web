46645564f1f26306187c2967d13aee9999cb4893
Anuwatchaturon <anuwat.pu.61@ubu.ac.th>
2021-02-24 04:19:02 +0700

    Updates

---
+++ precache-manifest.7f0d45f3eb1f2c9f40553a7e09dc40b0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded10b8b4a750b379d7d82e6ccd2a079",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "f3c1e9e0be961d3ba226",
+    "url": "/react-web/static/css/main.86033e3f.chunk.css"
+  },
+  {
+    "revision": "a93d81baf651a5a865b8",
+    "url": "/react-web/static/js/2.2b13a32b.chunk.js"
+  },
+  {
+    "revision": "ac4a31b929cb9380c4a242904540a469",
+    "url": "/react-web/static/js/2.2b13a32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c1e9e0be961d3ba226",
+    "url": "/react-web/static/js/main.0b3998ca.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/react-web/static/js/main.0b3998ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508ce6ce68595c250116",
+    "url": "/react-web/static/js/runtime-main.b5c3bfe7.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/react-web/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/react-web/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/react-web/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/react-web/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/react-web/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/react-web/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/react-web/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/react-web/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/react-web/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/react-web/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/react-web/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/react-web/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/react-web/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "aa712d017f1ed93314ff99d70e5006a6",
+    "url": "/react-web/static/media/me.aa712d01.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/react-web/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/react-web/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/react-web/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/react-web/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/react-web/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/react-web/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/react-web/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
